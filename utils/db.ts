d43d2abba26725da4c9a8900ec8ea643f7d24f9e
Siddhant-ops <siddhantdalvi3@gmail.com>
2023-02-02 11:42:17 +0530

    server setup

---
+++ utils/db.ts
@@ -0,0 +1,20 @@
+import { connect, set } from 'mongoose';
+
+const dbConnect = () => {
+  if (process.env.DATABASE_URL && process.env.DATABASE_PASSWORD) {
+    const mongoURL = process.env.DATABASE_URL.replace('<password>', process.env.DATABASE_PASSWORD);
+
+    const seperator = mongoURL.lastIndexOf('/');
+    const DB = mongoURL.slice(0, seperator + 1) + process.env.ENV + mongoURL.slice(seperator + 1);
+
+    set('strictQuery', true);
+    connect(DB, err => {
+      if (err) console.log(err);
+      else console.log('DB connection successfull!');
+    });
+  } else {
+    console.log('DB connection failed!');
+  }
+};
+
+export { dbConnect };
